2e4eb0c03af1d615eeb3908f0449650951b7abdb
Chris Wendel <chriwend@umich.edu>
2013-09-10 01:16:45 -0400

    Initial Commit

---
+++ SWTableViewCell/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SWTableViewCell
+//
+//  Created by Chris Wendel on 9/10/13.
+//  Copyright (c) 2013 Chris Wendel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SWTableViewCell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SWTableViewCell
+//
+//  Created by Chris Wendel on 9/10/13.
+//  Copyright (c) 2013 Chris Wendel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
