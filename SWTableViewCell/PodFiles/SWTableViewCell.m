8e6f8b0fc213cc87de425b8010b7715bdc1579a7
Chris Wendel <chriwend@umich.edu>
2014-02-17 21:29:40 -0500

    Reverting changes from #89

--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -482,15 +482,13 @@ static NSString * const kTableViewCellContentView = @"UITableViewCellContentView
         case kCellStateCenter:
             if (velocity.x >= 0.5f)
             {
-                if ([self rightUtilityButtonsWidth] > 0)
-                    [self scrollToRight:targetContentOffset];
+                [self scrollToRight:targetContentOffset];
             }
             else if (velocity.x <= -0.5f)
             {
-                if ([self leftUtilityButtonsWidth] > 0)
-                    [self scrollToLeft:targetContentOffset];
+                [self scrollToLeft:targetContentOffset];
             }
-            else if (velocity.x != 0)
+            else
             {
                 CGFloat rightThreshold = [self utilityButtonsPadding] - ([self rightUtilityButtonsWidth] / 2);
                 CGFloat leftThreshold = [self leftUtilityButtonsWidth] / 2;
@@ -569,11 +567,6 @@ static NSString * const kTableViewCellContentView = @"UITableViewCellContentView
             scrollViewBounds.origin.x = MAX([self rightUtilityButtonsWidth] - scrollViewWidth, [self rightUtilityButtonsWidth] - scrollView.contentOffset.x);
             self.scrollViewButtonViewRight.bounds = scrollViewBounds;
         }
-        else
-        {
-            [scrollView setContentOffset:CGPointMake([self leftUtilityButtonsWidth], 0)];
-            self.tapGestureRecognizer.enabled = YES;
-        }
     }
     else
     {
@@ -591,11 +584,6 @@ static NSString * const kTableViewCellContentView = @"UITableViewCellContentView
             CGFloat scrollViewWidth = MIN(scrollView.contentOffset.x - [self leftUtilityButtonsWidth], [self leftUtilityButtonsWidth]);
             
             self.scrollViewButtonViewLeft.frame = CGRectMake([self leftUtilityButtonsWidth], 0.0f, scrollViewWidth, self.height);
-        }
-        else
-        {
-            [scrollView setContentOffset:CGPointMake(0, 0)];
-            self.tapGestureRecognizer.enabled = YES;
         }
     }
 }
