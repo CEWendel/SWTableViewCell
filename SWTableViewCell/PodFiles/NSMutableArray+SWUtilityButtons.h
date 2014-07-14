55a9d655c15b86a811753e0e2fcf967b69aa7c85
Nash Tsai <nash.tsai@gmail.com>
2014-07-14 17:47:01 +0800

    have sw_addUtilityButtonWithColor return UIButton*

--- SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.h
+++ SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.h
@@ -7,18 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSMutableArray (SWUtilityButtons)
 
-- (void)sw_addUtilityButtonWithColor:(UIColor *)color title:(NSString *)title;
-- (void)sw_addUtilityButtonWithColor:(UIColor *)color icon:(UIImage *)icon;
-- (void)sw_addUtilityButtonWithColor:(UIColor *)color normalIcon:(UIImage *)normalIcon selectedIcon:(UIImage *)selectedIcon;
-
-@end
-
-
-@interface NSArray (SWUtilityButtons)
-
-- (BOOL)sw_isEqualToButtons:(NSArray *)buttons;
+- (UIButton*)sw_addUtilityButtonWithColor:(UIColor *)color title:(NSString *)title;
+- (UIButton*)sw_addUtilityButtonWithColor:(UIColor *)color icon:(UIImage *)icon;
+- (UIButton*)sw_addUtilityButtonWithColor:(UIColor *)color normalIcon:(UIImage *)normalIcon selectedIcon:(UIImage *)selectedIcon;
 
 @end
