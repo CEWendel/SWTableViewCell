b5e50e767526510eb3ce3c631e62e361422e82ec
Christopher Wendel <chriwend@umich.edu>
2014-10-26 18:22:16 -0400

    Adding CGFloat rounding to avoid layout issues. Fixes #253

--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -526,17 +526,29 @@ static NSString * const kTableViewPanState = @"state";
 
 - (CGFloat)leftUtilityButtonsWidth
 {
-    return CGRectGetWidth(self.leftUtilityButtonsView.frame);
+#if CGFLOAT_IS_DOUBLE
+    return round(CGRectGetWidth(self.leftUtilityButtonsView.frame));
+#else
+    return roundf(CGRectGetWidth(self.leftUtilityButtonsView.frame));
+#endif
 }
 
 - (CGFloat)rightUtilityButtonsWidth
 {
-    return CGRectGetWidth(self.rightUtilityButtonsView.frame) + self.additionalRightPadding;
+#if CGFLOAT_IS_DOUBLE
+    return round(CGRectGetWidth(self.rightUtilityButtonsView.frame) + self.additionalRightPadding);
+#else
+    return roundf(CGRectGetWidth(self.rightUtilityButtonsView.frame) + self.additionalRightPadding);
+#endif
 }
 
 - (CGFloat)utilityButtonsPadding
 {
-    return [self leftUtilityButtonsWidth] + [self rightUtilityButtonsWidth];
+#if CGFLOAT_IS_DOUBLE
+    return round([self leftUtilityButtonsWidth] + [self rightUtilityButtonsWidth]);
+#else
+    return roundf([self leftUtilityButtonsWidth] + [self rightUtilityButtonsWidth]);
+#endif
 }
 
 - (CGPoint)contentOffsetForCellState:(SWCellState)state
