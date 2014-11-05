201989e2396ea536d8f45fec100853de7225890e
Christopher Wendel <chriwend@umich.edu>
2014-11-05 10:45:36 -0500

    Reverting 047cd75

--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -242,6 +242,9 @@ static NSString * const kTableViewPanState = @"state";
         _leftUtilityButtons = leftUtilityButtons;
         
         self.leftUtilityButtonsView.utilityButtons = leftUtilityButtons;
+
+        [self.leftUtilityButtonsView layoutIfNeeded];
+        [self layoutIfNeeded];
     }
 }
 
@@ -250,6 +253,9 @@ static NSString * const kTableViewPanState = @"state";
     _leftUtilityButtons = leftUtilityButtons;
     
     [self.leftUtilityButtonsView setUtilityButtons:leftUtilityButtons WithButtonWidth:width];
+
+    [self.leftUtilityButtonsView layoutIfNeeded];
+    [self layoutIfNeeded];
 }
 
 - (void)setRightUtilityButtons:(NSArray *)rightUtilityButtons
@@ -258,6 +264,9 @@ static NSString * const kTableViewPanState = @"state";
         _rightUtilityButtons = rightUtilityButtons;
         
         self.rightUtilityButtonsView.utilityButtons = rightUtilityButtons;
+
+        [self.rightUtilityButtonsView layoutIfNeeded];
+        [self layoutIfNeeded];
     }
 }
 
@@ -266,6 +275,9 @@ static NSString * const kTableViewPanState = @"state";
     _rightUtilityButtons = rightUtilityButtons;
     
     [self.rightUtilityButtonsView setUtilityButtons:rightUtilityButtons WithButtonWidth:width];
+
+    [self.rightUtilityButtonsView layoutIfNeeded];
+    [self layoutIfNeeded];
 }
 
 #pragma mark - UITableViewCell overrides
